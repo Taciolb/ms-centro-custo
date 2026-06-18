53e090d1bee3456635601903c8d97e1add6c3571
Taciano <taciolb@gmail.com>
2026-06-17 22:01:15 -0300

    chore: atualizado o pom.xml com JWT e corrigida as dependencias

---
+++ src/main/java/br/com/controlefinanceiro/ms/centrocusto/model/CentroCusto.java
@@ -0,0 +1,10 @@
+package br.com.controlefinanceiro.ms.centrocusto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "centros_custo")
+
+public class CentroCusto {
+}
