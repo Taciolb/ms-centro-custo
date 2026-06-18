03930f9e2ad423abad74e83be9a147ed1b51a669
Taciano <taciolb@gmail.com>
2026-06-17 21:18:27 -0300

    feat: projeto ms-centro-custo iniciado

---
+++ src/main/java/br/com/controlefinanceiro/ms/centrocusto/MsCentroCustoApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.ms.centrocusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCentroCustoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCentroCustoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controlefinanceiro/ms/centrocusto/MsCentroCustoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro.ms.centrocusto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCentroCustoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
