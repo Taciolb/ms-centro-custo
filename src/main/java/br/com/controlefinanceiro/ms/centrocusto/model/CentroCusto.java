0e310333df4429c2ce7adaf92b9201c280955e0f
Taciano <taciolb@gmail.com>
2026-06-17 22:16:31 -0300

    feat: adiciona entity CentroCusto

--- src/main/java/br/com/controlefinanceiro/ms/centrocusto/model/CentroCusto.java
+++ src/main/java/br/com/controlefinanceiro/ms/centrocusto/model/CentroCusto.java
@@ -1,10 +1,50 @@
 package br.com.controlefinanceiro.ms.centrocusto.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "centros_custo")
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CentroCusto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(length = 255)
+    private String descricao;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @Column(name = "usuario_id", nullable = false)
+    private String usuarioId;
+
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+
+    @PrePersist
+    public void prePersist() {
+        this.criadoEm = LocalDateTime.now();
+        this.atualizadoEm = LocalDateTime.now();
+        this.ativo = true;
+    }
+
+    @PrePersist
+    public void preUpdate() {
+        this.atualizadoEm = LocalDateTime.now();
+    }
 }
